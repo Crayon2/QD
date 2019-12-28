25be7e37325c14c09c5dadeb2c57bf1aec8f6cb1
刘瑜鹏 <44955429+Crayon2@users.noreply.github.com>
2019-12-28 16:23:41 +0800

    Add files via upload

---
+++ qmks/src/App0.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+  	<header-nav></header-nav>	
+  	<router-view></router-view>
+  	<footer-nav></footer-nav>	
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+    headerNav,
+    footerNav
+  }
+}
+</script>
+
+<style>
+
+</style>
